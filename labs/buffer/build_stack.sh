e562181e98decc374178bf95b2d1ff7303295eac
gzhelezo <44788337+gzhelezo@users.noreply.github.com>
2021-02-17 20:57:18 -0500

    update build stack

--- labs/buffer/build_stack.sh
+++ labs/buffer/build_stack.sh
@@ -1,4 +1,4 @@
-gcc -DBUF_SIZE=108 -o stack -z execstack -fno-stack-protector stack.c
+gcc -DBUF_SIZE=212 -o stack -z execstack -fno-stack-protector stack.c
 sudo chown root stack
 sudo chmod 4755 stack
 /bin/ls -l
